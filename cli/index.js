f16929a3f3cc894ab79e5caa97d0a919533330b0
David Marrs <david@marrsbytes.com>
2022-05-08 21:52:07 +0100

    Fix cli after conversion to ES modules

--- cli/index.js
+++ cli/index.js
@@ -2,7 +2,7 @@
 import fs from 'fs'
 import { StringDecoder } from 'string_decoder'
 import { Buffer } from 'buffer';
-import { init_parse_state, parse_chunk } from '../src/parser'
+import { init_parse_state, parse_chunk } from '../src/parser.js'
 
 var idxFilename = (/node$/.test(process.argv[0]))? 2 : 1;
 var filename = process.argv[idxFilename];
@@ -16,13 +16,13 @@ var readStream = fs.createReadStream(filename);
 var parseState, decoder;
 
 readStream.on('open', function() {
-    parseState = parser.init_parse_state();
+    parseState = init_parse_state();
     decoder = new StringDecoder('utf8');
 });
 
 readStream.on('data', function(chunk) {
     var result = [];
-    parser.parse_chunk(decoder.write(chunk), result, parseState)
+    parse_chunk(decoder.write(chunk), result, parseState)
     parseState.lastChunk = chunk;
     process.stdout.write(Buffer.from(result.join('')));
 });
